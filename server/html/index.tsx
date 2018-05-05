fbfcf72ebfa7d558f99e4adaa7e07ac269faded4
Dillon Jason <djasuar@gmail.com>
2018-05-05 14:09:44 -0400

    fixed index prop

--- server/html/index.tsx
+++ server/html/index.tsx
@@ -8,7 +8,7 @@ export const Html = (props: HtmlProps) => {
     return (
         <html>
             <head>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <title>Nick Gonzalez</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </head>
