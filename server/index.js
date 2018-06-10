61ff6a021fc05c385ff51b26578c085e128b793a
Dillon Jason <djasuar@gmail.com>
2018-06-10 17:48:47 -0400

    Fixed html path

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080
 app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/prod.html'));
+  res.sendFile(path.join(__dirname + '../prod.html'));
 })
 
 app.listen(port)
