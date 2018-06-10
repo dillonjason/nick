7f3a64b3b05632771ec587e7864edde3c3dea6ba
Dillon Jason <djasuar@gmail.com>
2018-06-10 18:45:48 -0400

    profile loading

--- src/components/profile/placeholder/index.jsx
+++ src/components/profile/placeholder/index.jsx
@@ -1,3 +1,18 @@
 import React from 'react'
 
-export const Placeholder = () => <div className='placeholder'>Loading...</div>
+import {profile_data, picture, content} from '../profile_data/styles.scss'
+import {header_bar, content_large, content_small, picture_block, content_block} from './styles.scss'
+
+export const Placeholder = () => (
+  <div className={profile_data}>
+    <div className={`${picture} ${picture_block}`} />
+    <div className={`${content} ${content_block}`}>
+      <div className={header_bar} />
+      <div className={content_large} />
+      <div className={content_large} />
+      <div className={content_large} />
+      <div className={content_large} />
+      <div className={content_small} />
+    </div>
+  </div>
+)
